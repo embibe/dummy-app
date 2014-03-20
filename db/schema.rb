dadc6ee1e474cea843944d0f7704c0854a2b5306
Anuj More <anujmorex@gmail.com>
2014-03-20 15:43:11 +0530

    Move to psql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140320084245) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "entries", force: true do |t|
     t.integer  "entry"
     t.datetime "created_at"
@@ -34,7 +37,7 @@ ActiveRecord::Schema.define(version: 20140320084245) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
