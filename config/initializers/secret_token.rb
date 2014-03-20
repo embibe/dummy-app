49efab38250a552276ba93bf56685241308297f9
Anuj More <anujmorex@gmail.com>
2014-03-20 14:10:38 +0530

    Devise working

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wasted::Application.config.secret_key_base = '52ef63b50bb2076e770de6ffab4f4f286b66266fab813eeb7cb82cc95d2d910aaa03366651176bf83de25a9bc3b3413b343d2b1bf5afbe9a9a1aaf80ec4ee8e5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wasted::Application.config.session_store :cookie_store, key: '_wasted_session'
